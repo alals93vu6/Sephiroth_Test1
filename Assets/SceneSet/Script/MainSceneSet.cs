d9f6f5c8ded06d19b0aa3523a5d0fb4c296f19cb
YouQi <alexjohn2109a@gmail.com>
2022-10-25 18:44:52 +0800

    Add SceneSet

---
+++ Assets/SceneSet/Script/MainSceneSet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneSet : MonoBehaviour
+{
+    [Tooltip("主要畫面編號")]
+    [Header("主要畫面編號")]
+    [SerializeField] private int MainSceneIdex;
+
+    [Tooltip("遊戲畫面編號")]
+    [Header("遊戲畫面編號")]
+    [SerializeField] private int GameSceneIdex;
+
+    public void LoadScene_MainScene()
+    {
+        SceneManager.LoadScene(MainSceneIdex);
+    }
+    public void LoadScene_GameScene()
+    {
+        SceneManager.LoadScene(GameSceneIdex);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
