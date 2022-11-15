ad5d719978eaa9068855dff078b0c4bb6b625d51
YouQi <alexjohn2109a@gmail.com>
2022-11-15 20:00:25 +0800

    Add Effect

---
+++ Assets/Effect/Particle System/DustTrail/DustTrail.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustTrail : MonoBehaviour
+{
+    [Header("特效")]
+    [SerializeField] private ParticleSystem DustTrail_particleSystem;
+    [Header("待機")]
+    [SerializeField] private bool DustTrail_standby = true;
+    [Header("左走")]
+    [SerializeField] private bool DustTrail_leftWalk = false;
+    [Header("右走")]
+    [SerializeField] private bool DustTrail_rightWalk = false;
+    [Header("煞車")]
+    [SerializeField] private bool DustTrail_brakes = false;
+    [Header("跳躍")]
+    [SerializeField] private bool DustTrail_jump = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Effect/Shader/Disappear/Disappear.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+    [Header("設定消失渲染")]
+    [SerializeField] public Material DisappearMaterial;
+    [Header("是否消失")]
+    [SerializeField] public bool Disappear_is = false; //正常出現
+    [Header("當前消失進度")]
+    [SerializeField] public float Disappear_now;
+    [Header("消失進度")]
+    [SerializeField] public float Disappear_new;
+    [Header("消失速度")]
+    [SerializeField] public float Disappear_Speed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisappearMaterial.SetFloat("IsDisappear", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetDisappear();
+        disappear_lerp_down();
+        disappear_lerp_up();
+        SetDisappear();
+    }
+    void GetDisappear()
+    {
+        Disappear_now = DisappearMaterial.GetFloat("IsDisappear");
+    }
+
+    void SetDisappear()
+    {
+        DisappearMaterial.SetFloat("IsDisappear", Disappear_new);
+    }
+    void disappear_lerp_down()
+    {
+        if (Disappear_is == true)
+        {
+            Disappear_new = Mathf.Lerp(Disappear_now, 0f, Disappear_Speed * Time.deltaTime);
+        }
+    }
+    void disappear_lerp_up()
+    {
+        if (Disappear_is == false)
+        {
+            Disappear_new = Mathf.Lerp(Disappear_now, 1f, Disappear_Speed * Time.deltaTime);
+        }
+    }
+
+}
