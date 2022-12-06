379e210e13456e85e373b9275b7d04532c0880c7
alals93vu6 <a0987915131@gmail.com>
2022-12-07 04:40:10 +0800

    Map Portal

---
+++ Assets/Scirpt/GamePlaying/AreaOBJ/MapPortal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPortal : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject endPoint;
+    [SerializeField] private Vector2 portalSize;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics2D.OverlapBox(this.transform.position, portalSize, 0, 4096))
+        {
+            player.transform.position = endPoint.transform.position;
+        }
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        
+        Gizmos.DrawWireCube(this.transform.position,portalSize);
+    }
+}
