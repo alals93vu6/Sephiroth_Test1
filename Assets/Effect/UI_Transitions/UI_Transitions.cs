f20d6ddaabd1ea34423c08b3286d22c350067927
YouQi <alexjohn2109a@gmail.com>
2022-12-09 01:18:28 +0800

    Add UI_Transitions

---
+++ Assets/Effect/UI_Transitions/UI_Transitions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Transitions : MonoBehaviour
+{
+    public Animator animator;
+    public bool is_start;
+    public bool is_end;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("Is_Transitions_end",is_end);
+        animator.SetBool("Is_Transitions_start",is_start);
+    }
+}
