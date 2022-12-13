9dd2c31025dae96bdb205e85fd75b984525e51bb
YouQi <alexjohn2109a@gmail.com>
2022-12-13 23:54:02 +0800

    Add new UI script

--- Assets/Scirpt/SceneSet/Script/MainSceneSet.cs
+++ Assets/Scirpt/SceneSet/Script/MainSceneSet.cs
@@ -7,34 +7,59 @@ public class MainSceneSet : MonoBehaviour
 {
     [Tooltip("載入畫面編號")]
     [Header("載入畫面編號")]
-    [SerializeField] private int Scene00Idex;
+    [SerializeField] private List<int> SceneIdex = new List<int>();
 
-    [Tooltip("載入畫面編號")]
-    [Header("載入畫面編號")]
-    [SerializeField] private int Scene01Idex;
-    
-    [Tooltip("載入畫面編號")]
-    [Header("載入畫面編號")]
-    [SerializeField] private int Scene02Idex;
+    [Header("按鈕當前位置")]
+    [Range(0, 10)][SerializeField] public int MainSceneNum;
+    [Header("提示提示位置")]
+    [SerializeField] public List<Vector3> TipRectTransform = new List<Vector3>();
+    [Header("提示物件")]
+    [SerializeField] public RectTransform TipGameObject;
 
+    private void Start()
+    {
+        MainSceneNum = 0;
+    }
+    private void Update()
+    {
+        Debug.Log(TipGameObject.localPosition);
+        Chang_TipTransform(TipGameObject.localPosition,5);
+    }
     public void LoadScene_Scene00()
     {
-        SceneManager.LoadScene(Scene00Idex);
+        SceneManager.LoadScene(SceneIdex[0]);
         Cursor.visible = false;
         SetAudio.audioNum = 1;
     }
     public void LoadScene_Scene01()
     {
-        SceneManager.LoadScene(Scene01Idex);
+        SceneManager.LoadScene(SceneIdex[1]);
     }
 
     public void LoadScene_Scene02()
     {
-        SceneManager.LoadScene(Scene02Idex);
+        SceneManager.LoadScene(SceneIdex[2]);
     }
 
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    void Chang_TipTransform(Vector3 now_vect3, float speed)
+    {
+        now_vect3 = TipGameObject.localPosition;
+        
+        TipGameObject.localPosition = Vector3.Lerp(now_vect3, TipRectTransform[MainSceneNum], speed * Time.deltaTime);
+
+
+        if (MainSceneNum > TipRectTransform.Count)
+        {
+            MainSceneNum = 0;
+        }
+        if (MainSceneNum < 0)
+        {
+            MainSceneNum = 0;
+        }
     }
 }
